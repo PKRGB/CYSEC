650da0735ce11deafb8defd0aa2fc184161adbc3
PKRGB <pkay2022@gmail.com>
2026-01-12 00:32:07 -0600

    Create accordion functionality in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Accordion functionality
+const accordionHeader = document.querySelector('.accordion-header');
+if (accordionHeader) {
+  accordionHeader.addEventListener('click', function() {
+    // ... accordion code
+  });
+}
