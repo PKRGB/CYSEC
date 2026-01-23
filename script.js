cdc3e3d0389fae0a56fd43762874d3554b18d66d
PKRGB <pkay2022@gmail.com>
2026-01-23 00:01:49 -0600

    Simplify accordion header selector

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Accordion functionality //
-const accordionHeader = document.querySelector('.accordion-wrapper .accordion-header');
+const accordionHeader = document.querySelector('.accordion-header');
 if (accordionHeader) {
   accordionHeader.addEventListener('click', function() {
     const content = this.nextElementSibling;
